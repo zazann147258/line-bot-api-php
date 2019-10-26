bfab7b3ce9482d68d3cfe80aa107ed43a9fe0393
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2019-10-27 01:40:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -4,16 +4,17 @@
 
 // Get POST body content
 $content = file_get_contents('php://input');
-// Parse JSON
 
+// Parse JSON
 $events = json_decode($content, true);
+
 // Validate parsed JSON data
 if (!is_null($events['ESP'])) {
-	
 	send_LINE($events['ESP']);
 		
 	echo "OK";
-	}
+}
+
 if (!is_null($events['events'])) {
 	echo "line bot";
 	// Loop through each event
@@ -33,5 +34,7 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
+
 echo "OK3";
+
 ?>
